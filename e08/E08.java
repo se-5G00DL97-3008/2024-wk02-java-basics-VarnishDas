210fd78c4c1f0f648f804018cfad9d2423cd9caa
Varnish Das <varnish.das@outlook.com>
2024-01-18 01:06:15 +0200

    e08

--- e08/E08.java
+++ e08/E08.java
@@ -2,6 +2,21 @@ import java.util.Scanner;
 
 class E08 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            System.out.println("Give a number (0 to quit):");
+            int num = scanner.nextInt();
+
+            if (num == 0) {
+                break;
+            } else if (num % 2 == 0) {
+                System.out.println("Number is even");
+            } else {
+                System.out.println("Number is odd");
+            }
+        }
+
+        scanner.close();
     }
 }

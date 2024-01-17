10661812fcfa8908758edd01cc945eb23ebe95ac
Varnish Das <varnish.das@outlook.com>
2024-01-18 00:29:44 +0200

    e03

--- e03/E03.java
+++ e03/E03.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 class E03 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Enter a number:: ");
+        int num = scanner.nextInt();
+
+        if (num > 0) {
+            System.out.println("Positive number");
+        } else if (num < 0) {
+            System.out.println("Negative number");
+        } else {
+            System.out.println("Zero");
+        }
+
+        scanner.close();
     }
 }

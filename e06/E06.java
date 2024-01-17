ae65299242cd48f5ca1e64fa538896c244838f0e
Varnish Das <varnish.das@outlook.com>
2024-01-18 00:50:56 +0200

    e06

--- e06/E06.java
+++ e06/E06.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 class E06 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("0AWhich multiplication table to show?");
+        int num = scanner.nextInt();
+
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i + " x " + num + " = " + num * i);
+        }
+
+        scanner.close();
     }
 }

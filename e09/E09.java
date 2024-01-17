597ed111fd7d5db1663c0417434c36d7cc9c8b64
Varnish Das <varnish.das@outlook.com>
2024-01-18 01:18:21 +0200

    e09

--- e09/E09.java
+++ e09/E09.java
@@ -4,7 +4,7 @@ class E09 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int total = 0;
+        double total = 0;
         int count = 0;
 
         while (true) {

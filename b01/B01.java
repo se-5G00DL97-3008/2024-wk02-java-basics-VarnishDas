b9ecac1739d43e5c983e796ab3d8f3e9b77939d2
Varnish Das <varnish.das@outlook.com>
2024-01-18 01:32:46 +0200

    b01

--- b01/B01.java
+++ b01/B01.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 class B01 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Enter the starting count of bottles: ");
+        int bottles = scanner.nextInt();
+        int ogBottles = bottles;
+
+        while (bottles > 0) {
+            System.out.println(bottles + " bottles of beer on the wall, " + bottles + " bottles of beer.");
+            bottles--;
+
+            if (bottles == 0) {
+                System.out.println("No more bottles of beer on the wall, no more bottles of beer.");
+                System.out.println("Go to the store and buy some more, " + ogBottles + " bottles of beer on the wall.");
+            }
+            else {
+                System.out.println("Take one down and pass it around, " + bottles + " bottles of beer on the wall.");
+            }
+        }
+
+
+
+        scanner.close();
     }
 }

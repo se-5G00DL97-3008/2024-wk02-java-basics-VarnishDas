e448c257642cdeb8d1ee693837d00baa0c8d60a3
Varnish Das <varnish.das@outlook.com>
2024-01-18 00:17:54 +0200

    e02

--- e02/E02.java
+++ e02/E02.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 class E02 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Give a number:");
+        int num1 = scanner.nextInt();
+
+        System.out.println("Give another number:");
+        int num2 = scanner.nextInt();
+
+        int sum = num1 + num2;
+        int multiply = num1 * num2;
+        int subtract = num1 - num2;
+        int divide = num1 / num2;
+
+        System.out.println(num1 + " + " + num2 + " = " + sum);
+        System.out.println(num1 + " * " + num2 + " = " + multiply);
+        System.out.println(num1 + " - " + num2 + " = " + subtract);
+        System.out.println(num1 + " / " + num2 + " = " + divide);
     }
 }
